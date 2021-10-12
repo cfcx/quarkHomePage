6ced0947d0febef746fbca90df8032c3973cac90
cfcx <91699434+cfcx@users.noreply.github.com>
2021-10-12 17:26:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('https://g.alicdn.com/kg/workbox/4.3.1/workbox-sw.js');
 workbox.routing.registerRoute(/([\/|.|\w|\s|-])*\.(?:png|gif|jpg|jpeg|webp|svg|ico|ttf|woff|woff2|eot|otf|js|css)/, new workbox.strategies.CacheFirst({
     cacheName: 'static-resources',
     plugins: [new workbox.expiration.Plugin({
-        maxEntries: 500, // 最大保存项目
+        maxEntries: 1000, // 最大保存项目
         maxAgeSeconds: 7 * 24 * 60 * 60, // 缓存 1 天
         purgeOnQuotaError: true
     })]
